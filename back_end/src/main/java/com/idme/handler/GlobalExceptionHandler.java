8b2993aadbb9fe5465acb7a51c265eb30c51cd0c
Chen1shark <1943086187@qq.com>
2025-06-03 10:54:16 +0800

    异常处理

---
+++ back_end/src/main/java/com/idme/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.idme.handler;
+
+import com.idme.exception.BaseException;
+import com.idme.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理器，处理项目中抛出的业务异常
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获业务异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(BaseException ex){
+        return Result.error(ex.getMessage());
+    }
+
+}
