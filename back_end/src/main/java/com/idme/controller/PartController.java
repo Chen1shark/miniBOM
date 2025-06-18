f5cfb16c04ba3a9ac17617dc27fa2fc2d3917599
2506842763@qq.com <2506842763@qq.com>
2025-06-18 16:26:51 +0800

    提交

--- back_end/src/main/java/com/idme/controller/PartController.java
+++ back_end/src/main/java/com/idme/controller/PartController.java
@@ -1,15 +1,16 @@
 package com.idme.controller;
 
 import com.huawei.innovation.rdm.minibom.dto.entity.PartCreateDTO;
+import com.huawei.innovation.rdm.minibom.dto.entity.PartQueryViewDTO;
+import com.huawei.innovation.rdm.minibom.dto.entity.PartViewDTO;
 import com.idme.result.Result;
 import com.idme.service.PartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.idme.pojo.dto.PartBuildDto;
+
+import java.util.List;
 
 @Slf4j
 @RequestMapping("/part")
@@ -19,9 +20,21 @@ public class PartController {
     @Autowired
     PartService partService;
 
+
+    /**
+     * 添加part（此时未添加分类）
+     * @param partBuildDto
+     * @return id
+     */
     @PostMapping("/create")
-    public Result register(@RequestBody PartBuildDto partBuildDto){
+    public Result creat(@RequestBody PartBuildDto partBuildDto){
         Long partId = partService.createPart(partBuildDto);
         return Result.success(partId);
+    }
+
+    @GetMapping("/query")
+    public Result query(){
+        List<PartQueryViewDTO> view = partService.query();
+        return Result.success(view);
     }
 }
